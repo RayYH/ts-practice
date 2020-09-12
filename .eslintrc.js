f13bee1f21f77eb55bde47a786b64c76186c6c3b
rayyh <rayyounghong@gmail.com>
2020-09-12 09:37:12 +0800

    config file cs fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
     // disable warning: Unexpected any. Specify a different type
-    "@typescript-eslint/no-explicit-any": 0,
+    '@typescript-eslint/no-explicit-any': 0,
     // allowed @ts-ignore
-    "@typescript-eslint/ban-ts-ignore": 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
     // allow require
-    "@typescript-eslint/no-var-requires": 0,
+    '@typescript-eslint/no-var-requires': 0,
     // object
-    "@typescript-eslint/ban-types": 0,
+    '@typescript-eslint/ban-types': 0,
     // allow !. syntax
-    "@typescript-eslint/no-non-null-assertion": 0
+    '@typescript-eslint/no-non-null-assertion': 0,
   },
 };
